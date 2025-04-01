2b90d89cf97353521ff6629dd7b41750a8eab793
Lyu0715 <pegan2001@gmail.com>
2025-04-01 14:34:19 +0900

    cu

--- practice_.py
+++ practice_.py
@@ -1,7 +1,12 @@
-x=100
-y=100
+text = 'Python_Data-Analysis!'
 
-print( x == y )
+모음들 = "AEIOUaeiou"
+모음모음 = ""
 
-x= 7
-print(x == y)
+for char in text:
+    if char in 모음들:
+        모음모음 += char
+    else:
+        print(f"{char}은 모음 아님")
+
+print("모음들",모음모음)
