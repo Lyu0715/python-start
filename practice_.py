abe80936adfa039ede1fb348d25bc872bc65513e
Lyu0715 <pegan2001@gmail.com>
2025-04-01 19:10:56 +0900

    cumit

--- practice_.py
+++ practice_.py
@@ -1,16 +1,10 @@
-text = 'Python_Data-Analysis!'
-완성전문자 = ""
+one2nine = '123456789'
 
-for char in text:
-    if char in text: 
-        if char.isalpha():
-            완성된문자 += 완성전문자
-        else:
-            쓰레기 +=  char    
+#인풋값을 적으면 10이하로 숫자의 배수의 구구단을 다 나오게.
+
+숫자입력 = int(input())
 
 
-쓰레기 = ""
-완성된문자 = ""
-
-print(f"기타문자모음:{쓰레기}\n")
-("포함된 자음{완성된문자}")
+for char in one2nine:
+    A = 숫자입력 * char
+    print(f"숫자입력 * {char} 은 {A} 입니다.")
