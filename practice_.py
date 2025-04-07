c66170f1c62c2ee09bbfe3550607f5a4e8dbafac
LYU <pegan2001@gmail.com>
2025-04-07 21:35:56 +0900

    1

--- practice_.py
+++ practice_.py
@@ -1,13 +1,13 @@
-# one2nine = '123456789'
+one2nine = '123456789'
 
 #인풋값을 적으면 10이하로 숫자의 배수의 구구단을 다 나오게.
  
-# 숫자입력 = int(input())
+숫자입력 = int(input())
 
 
-# for char in one2nine:
-#     A = 숫자입력 * char
-#     print(f"숫자입력 * {char} 은 {A} 입니다.")
+for char in one2nine:
+    A = 숫자입력 * char
+    print(f"숫자입력 * {char} 은 {A} 입니다.")
 
 
 def 변수(left , right):
